bd9ce9a2ac28218f62bb57cab957b3a99b736849
ccorbin <calcorbin@gmail.com>
2025-03-28 13:38:29 -0500

    Create user model

---
+++ models/user.go
@@ -0,0 +1,34 @@
+package models
+
+import (
+	"errors"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	gorm.Model
+	Username string `json:"username" gorm:"unique"`
+	Password string `json:"-"`
+	Role     string `json:"role"`
+}
+
+func (u *User) HashPassword(password string) error {
+	if password == "" {
+		return errors.New("password cannot be empty")
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+
+	if err != nil {
+		return err
+	}
+
+	u.Password = string(hashedPassword)
+	return nil
+}
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
+}
